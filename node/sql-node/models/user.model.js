1a25238c94e28fde244cfc8ae8cf72ce368796d4
Simon Hoyos <shmesa23@gmail.com>
2020-10-01 10:43:18 -0500

    Sequelize first steps

---
+++ node/sql-node/models/user.model.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const userSchema = {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.BIGINT,
+    },
+    name: {
+      type: DataTypes.STRING,
+      noEmpty: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      notEmpty: true,
+    }
+  };
+
+  const userOps = {
+    timestamps: true,
+    tableName: 'users'
+  };
+
+  const User = sequelize.define('User', userSchema, userOps);
+
+  return User;
+}
