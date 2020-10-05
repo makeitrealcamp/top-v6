966e35f9a5683a9dca2588f71a420483ffc25ad6
Simon Hoyos <shmesa23@gmail.com>
2020-10-05 10:08:08 -0500

    JS Tooling

---
+++ javascript/js-tooling/index.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-console
+console.log('hola mundo');
+
+'hola mundo'
+  .split(' ')
+  .split('')
+  .filter((el) => el == 'a')
+  .map(() => 2)
+  .reduce((acc, el) => acc + el, 0);
